fdd4d20d7c72d1fb16b027fe3f3a2cf700fb7837
patrick pu <pxlxinzhao126@gmail.com>
2021-03-09 14:18:30 -0500

    firebase works!

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBjCwaZ7w9FuBmR56uuF6o6V_pNSluiHoU',
+  authDomain: 'bitcheq.firebaseapp.com',
+  databaseURL: 'https://bitcheq-default-rtdb.firebaseio.com',
+  projectId: 'bitcheq',
+  storageBucket: 'bitcheq.appspot.com',
+  messagingSenderId: '1084343878879',
+  appId: '1:1084343878879:web:d16d614e928009801cd56a',
+  measurementId: 'G-SKZSQZ0WL0'
+};
+
+firebase.initializeApp(firebaseConfig);
 
 @Component({
   selector: 'app-tab1',
@@ -8,14 +24,21 @@ import { Component, OnInit } from '@angular/core';
 export class Tab1Page implements OnInit{
   email: string;
   password: string;
+  // firebase: any;
 
   constructor() {}
 
   ngOnInit() {
+    /* tslint:disable:no-string-literal */
+    // this.firebase = window['firebase'];
   }
 
   login() {
-    console.log('email', this.email);
-    console.log('password', this.password);
+    firebase.auth().signInWithEmailAndPassword('p@p.com', '123456')
+    .then((userCredential) => {
+      console.log('userCredential', userCredential);
+    })
+    .catch((error) => {
+    });
   }
 }
