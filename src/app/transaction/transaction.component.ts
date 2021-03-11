516ee4872f020d5cd85c61202586390d3d10b29a
patrick pu <pxlxinzhao126@gmail.com>
2021-03-10 23:46:57 -0500

    add tx details

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { TransactionService } from './transaction.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-transaction',
@@ -11,6 +12,7 @@ import { TransactionService } from './transaction.service';
 export class TransactionComponent implements OnInit {
   private sub: Subscription;
   private id: string;
+  tx: any;
 
   constructor(
     private router: Router,
@@ -18,7 +20,22 @@ export class TransactionComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    console.log(this.transactionService.getSelectedTx());
+    this.tx = this.transactionService.getSelectedTx();
+    if (!this.tx) {
+      this.router.navigate(['']);
+    }
+  }
+
+  shortId() {
+    if (this.tx) {
+      const txid = this.tx?.txid as string;
+      const len = txid.length;
+      return `${txid.substring(0, 8)}...${txid.substring(len - 8, len)}`;
+    }
+  }
+
+  fullDate() {
+    return moment(this.tx?.createdDate).format('MM/DD/YYYY h:mm:ss a Z');
   }
 
   back() {
