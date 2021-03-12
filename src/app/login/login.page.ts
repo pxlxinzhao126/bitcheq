6638598b0e04221743e429203062962a308cd85c
patrick pu <pxlxinzhao126@gmail.com>
2021-03-11 22:19:57 -0500

    add more form validation

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { formatISO } from 'date-fns';
 import { FirebaseService } from '../serivce/firebase.service';
 
 @Component({
@@ -12,6 +11,14 @@ export class LoginPage {
   password = '123456';
   password2: string;
   state = 'login';
+  submitted = false;
+  errorCode = null;
+  errorTypes = [
+    'auth/email-already-in-use',
+    'auth/wrong-password',
+    'auth/user-not-found',
+    'auth/too-many-requests'
+  ];
 
   constructor(private firebaseService: FirebaseService) {}
 
@@ -21,9 +28,12 @@ export class LoginPage {
     } else if (this.state === 'signUp') {
       this.state = 'login';
     }
+    this.submitted = false;
+    this.errorCode = null;
   }
 
   async login() {
+    this.submitted = true;
     if (this.email && this.password) {
       try {
         const user = await this.firebaseService.signInWithEmailAndPassword(
@@ -33,23 +43,38 @@ export class LoginPage {
         console.log('login success', user);
       } catch (e) {
         console.error('login error', e);
+        this.errorCode = e.code;
       }
     }
   }
 
   async signUp() {
-    if (this.email && this.password) {
-      if (this.password === this.password) {
-        try {
-          const userCredential = await this.firebaseService.createUserWithEmailAndPassword(
-            this.email,
-            this.password,
-          );
-          console.log('signUp success', userCredential.user);
-        } catch (e) {
-          console.error('login error', e);
-        }
+    this.submitted = true;
+    if (this.email && this.password && this.password === this.password2) {
+      try {
+        const userCredential = await this.firebaseService.createUserWithEmailAndPassword(
+          this.email,
+          this.password,
+        );
+        console.log('signUp success', userCredential.user);
+      } catch (e) {
+        console.error('login error', e);
+        this.errorCode = e.code;
       }
     }
+  }
+
+  isValidEmail() {
+    return (
+      this.email?.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/g)?.length > 0
+    );
+  }
+
+  clearErrorCode() {
+    this.errorCode = null;
+  }
+
+  isUnknownError() {
+    return this.errorTypes.indexOf(this.errorCode) === -1;
   }
 }
