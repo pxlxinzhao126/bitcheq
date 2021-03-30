062c6aa0412cc6c0ba33e736ba36cd204da0b7fc
patrick pu <pxlxinzhao@gmail.com>
2021-03-29 21:55:10 -0400

    improve send estimation

--- src/app/home/review/review.component.ts
+++ src/app/home/review/review.component.ts
@@ -19,12 +19,17 @@ export class ReviewComponent implements OnInit {
   sendingSuccess = false;
   sendingError = false;
 
+  estimation = 0;
+  estimationStatus = 'unknown';
+
   constructor(private router: Router,
     private sendSerivce: SendService,
     ) {
    }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.estimate();
+  }
 
   cancel() {
     this.router.navigate(['tabs', 'home']);
@@ -43,6 +48,30 @@ export class ReviewComponent implements OnInit {
     this.withdraw();
   }
 
+  private estimate() {
+    this.sendSerivce.estimate(+this.amount, this.address).subscribe(
+      (res) => {
+        console.log(res);
+        if (res['status'] === 'success') {
+          this.estimation = res['data'].estimated_network_fee;
+          this.estimationStatus = res['status'];
+          console.log('this.estimation', this.estimation);
+          console.log('this.estimationStatus', this.estimationStatus);
+        } else {
+          this.estimation = res['data'].data.estimated_network_fee;
+          this.estimationStatus = res['data'].status;
+          console.log('this.estimation', this.estimation);
+          console.log('this.estimationStatus', this.estimationStatus);
+        }
+
+
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
   private withdraw() {
     this.sendSerivce.withdraw(+this.amount, this.address).subscribe(
       (res) => {
@@ -55,5 +84,9 @@ export class ReviewComponent implements OnInit {
         this.sendSerivce.broadcast('error');
       }
     );
+  }
+
+  private getPriceImpact() {
+    return Math.round(this.estimation/+this.amount * 100 * 100)/100 + '%';
   }
 }
